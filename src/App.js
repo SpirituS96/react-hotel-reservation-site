11d165c9497a8715a2714081b0e4cb492f235a85
SpirituS96 <panfyorov.yura@gmail.com>
2021-11-24 19:19:00 +0700

    add navbar component and routing

--- src/App.js
+++ src/App.js
@@ -1,15 +1,23 @@
-import React from 'react';
-import './App.css';
-import Error from './pages/Error';
-import Home from './pages/Home';
-import Rooms from './pages/Rooms';
-import SingleRoom from './pages/SingleRoom';
+import React from "react";
+import "./App.css";
+import Error from "./pages/Error";
+import Home from "./pages/Home";
+import Rooms from "./pages/Rooms";
+import SingleRoom from "./pages/SingleRoom";
+import { Route, Routes } from "react-router-dom";
 
+import Navbar from './components/Navbar';
 
 function App() {
   return (
     <>
-    hello
+      <Navbar />
+      <Routes>
+        <Route exact path='/' element={<Home />} />
+        <Route exact path='/rooms' element={<Rooms />} />
+        <Route exact path='/rooms/:slug' element={<SingleRoom />} />
+        <Route exact path='*' element={<Error />} />
+      </Routes>
     </>
   );
 }
