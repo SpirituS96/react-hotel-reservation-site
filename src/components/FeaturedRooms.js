d2c5843d3df85018af11dc2528e63229796efafa
SpirituS96 <panfyorov.yura@gmail.com>
2021-12-05 18:25:57 +0700

    add rooms context

--- src/components/FeaturedRooms.js
+++ src/components/FeaturedRooms.js
@@ -4,8 +4,9 @@ import { RoomContext } from "../context";
 export default class FeaturedRooms extends Component {
   static contextType = RoomContext;
   render() {
-    const {name,greeting} = this.context;
-
-    return <div>{greeting} {name} from featured rooms</div>;
+    //const context = this.context;
+    const { featuredRooms: rooms } = this.context;
+    
+    return <div> from featured rooms</div>;
   }
 }
