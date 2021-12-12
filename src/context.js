d6a27dc121d8bac352ca09cb516dc55934aaf485
SpirituS96 <panfyorov.yura@gmail.com>
2021-12-12 23:59:28 +0700

    set up SingleRoom page, add getRoom function for room context provider

--- src/context.js
+++ src/context.js
@@ -2,8 +2,6 @@ import React, { Component } from "react";
 import items from "./data";
 const RoomContext = React.createContext();
 
-
-
 class RoomProvider extends Component {
   state = {
     rooms: [],
@@ -37,9 +35,15 @@ class RoomProvider extends Component {
     return tempItems;
   }
 
+  getRoom = (slug) => {
+    let tempRooms = [...this.state.rooms];
+    const room = tempRooms.find((room) => room.slug === slug);
+    return room;
+  };
+
   render() {
     return (
-      <RoomContext.Provider value={{ ...this.state }}>
+      <RoomContext.Provider value={{...this.state, getRoom: this.getRoom }}>
         {this.props.children}
       </RoomContext.Provider>
     );
--- src/pages/SingleRoom.js
+++ src/pages/SingleRoom.js
@@ -1,15 +1,62 @@
 import React, { Component } from "react";
-// import Banner from "../components/Banner";
-// import Hero from "../components/Hero";
+import defaultBcg from "../images/room-11.jpeg";
+import Banner from "../components/Banner";
+import Hero from "../components/Hero";
+import { Link, useParams } from "react-router-dom";
+import { RoomContext } from "../context";
 
-export default class SingleRoom extends Component {
+const withRouter = (WrappedComponent) => (props) => {
+  const params = useParams();
+
+  return <WrappedComponent {...props} params={params} />;
+};
+
+class SingleRoom extends Component {
+  constructor(props) {
+    super(props);
+    // console.log(this);
+    this.state = {
+      slug: this.props.params.slug,
+      defaultBcg,
+    };
+  }
+  static contextType = RoomContext;
+  // componentDidMount() {}
   render() {
+    const { getRoom } = this.context;
+    const room = getRoom(this.state.slug);
+    if (!room) {
+      return (
+        <div className='error'>
+          <h3>no such room could be found...</h3>
+          <Link to='/rooms' className='btn-primary'>
+            back to rooms
+          </Link>
+        </div>
+      );
+    }
+    const {
+      name,
+      description,
+      capacity,
+      size,
+      price,
+      extras,
+      breakfast,
+      pets,
+      images,
+    } = room;
     return (
-      //   <Hero>
-      //     <Banner title='Room' subtitle='single room'></Banner>
-      //   </Hero>
-
-      <div>Hello from single room</div>
+      <Hero hero='roomsHero'>
+        <Banner title={`${name} room`}>
+          <Link to='/rooms' className='btn-primary'>
+            back to rooms
+          </Link>
+        </Banner>
+      </Hero>
     );
   }
 }
+
+export default withRouter(SingleRoom);
+// SingleRoom.contextType = RoomContext;
