b2d8308081556861d9d9417bbddb4c4304e5a4a4
SpirituS96 <panfyorov.yura@gmail.com>
2022-02-21 12:21:03 +0700

    create StyledHero, add SingleRoom description

--- src/pages/SingleRoom.js
+++ src/pages/SingleRoom.js
@@ -4,6 +4,7 @@ import Banner from "../components/Banner";
 import Hero from "../components/Hero";
 import { Link, useParams } from "react-router-dom";
 import { RoomContext } from "../context";
+import StyledHero from "../components/StyledHero";
 
 const withRouter = (WrappedComponent) => (props) => {
   const params = useParams();
@@ -46,14 +47,51 @@ class SingleRoom extends Component {
       pets,
       images,
     } = room;
+
+    const [mainImg, ...defaultImg] = images; // destructuring array to get rest of the images in separate array
+
     return (
-      <Hero hero='roomsHero'>
-        <Banner title={`${name} room`}>
-          <Link to='/rooms' className='btn-primary'>
-            back to rooms
-          </Link>
-        </Banner>
-      </Hero>
+      <>
+        <StyledHero img={mainImg || this.state.defaultBcg}>
+          <Banner title={`${name} room`}>
+            <Link to='/rooms' className='btn-primary'>
+              back to rooms
+            </Link>
+          </Banner>
+        </StyledHero>
+        <section className='single-room'>
+          <div className='single-room-images'>
+            {defaultImg.map((item, index) => {
+              return <img key={index} src={item} alt={name} />;
+            })}
+          </div>
+          <div className='single-room-info'>
+            <article className='desc'>
+              <h3>details</h3>
+              <p>{description}</p>
+            </article>
+            <article className='info'>
+              <h3>info</h3>
+              <h6>price : ${price}</h6>
+              <h6>size : {size} SQFT</h6>
+              <h6>
+                max capacity :{" "}
+                {capacity > 1 ? `${capacity} people` : `${capacity} person`}
+              </h6>
+              <h6>{pets ? "pets allowed" : "no pets allowed"}</h6>
+              <h6>{breakfast && "free breakfast included"}</h6>
+            </article>
+          </div>
+        </section>
+        <section className="room-extras">
+          <h6>extras</h6>
+          <ul className="extras">
+            {extras.map((item,index)=>{
+              return <li key={index}>- {item}</li>
+            })}
+          </ul>
+        </section>
+      </>
     );
   }
 }
