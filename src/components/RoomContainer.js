0a47e8c093d4316de1bb4e1ca4dd5389b8b67f4d
SpirituS96 <panfyorov.yura@gmail.com>
2022-03-04 19:27:03 +0700

    add room filters

---
+++ src/components/RoomContainer.js
@@ -0,0 +1,45 @@
+import React from "react";
+import RoomFilter from "./RoomFilter";
+import RoomList from "./RoomList";
+import Loading from "./Loading";
+import { withRoomConsumer } from "../context";
+
+// providing context to RoomContainer by HOC withRoomConsumer
+
+function RoomContainer({ context }) {
+  const { loading, sortedRooms, rooms } = context;
+  if (loading) {
+    return <Loading />;
+  }
+  return (
+    <>
+      <RoomFilter rooms={rooms} />
+      <RoomList rooms={sortedRooms} />
+    </>
+  );
+}
+
+export default withRoomConsumer(RoomContainer);
+
+// import { RoomConsumer } from "../context";
+
+// providing context to RoomContainer by RoomConsumer
+// export default function RoomContainer() {
+//   return (
+//     <RoomConsumer>
+//       {(value) => {
+//         const { loading, sortedRooms, rooms } = value;
+//         if (loading) {
+//           return <Loading />;
+//         }
+//         return (
+//           <div>
+//             Hello from RoomContainer
+//             <RoomFilter rooms={rooms} />
+//             <RoomList rooms={sortedRooms} />
+//           </div>
+//         );
+//       }}
+//     </RoomConsumer>
+//   );
+// }
